1cd200c75b29f1007898e14ff2b1b5af79b6d28c
jardenliu <15915839707@163.com>
2019-07-22 15:49:54 +0800

    fix: 🌈workaround test error

--- jest.config.js
+++ jest.config.js
@@ -1,18 +1,18 @@
 module.exports = {
     verbose: false,
-    testURL: "http://localhost:8080/",
-    collectCoverage: true,
-    collectCoverageFrom: ["src/**"],
-    coveragePathIgnorePatterns: ["/__snapshots__/"],
+    testURL: 'http://localhost:8080/',
+    collectCoverage: false,
+    collectCoverageFrom: ['src/**'],
+    coveragePathIgnorePatterns: ['/__snapshots__/'],
     transform: {
-        "^.+\\.vue$": "vue-jest",
-        "^.+\\.jsx?$": "babel-jest",
-        ".+\\.(css|styl|less|sass|scss|svg|png|jpg|ttf|woff|woff2)$":
-            "jest-transform-stub"
+        '^.+\\.vue$': 'vue-jest',
+        '^.+\\.jsx?$': 'babel-jest',
+        '.+\\.(css|styl|less|sass|scss|svg|png|jpg|ttf|woff|woff2)$':
+            'jest-transform-stub'
     },
     moduleNameMapper: {
-        "@/(.*)$": "<rootDir>/src/$1"
+        '@/(.*)$': '<rootDir>/src/$1'
     },
-    moduleFileExtensions: ["js", "vue"],
-    snapshotSerializers: ["jest-serializer-vue"]
-};
+    moduleFileExtensions: ['js', 'vue'],
+    snapshotSerializers: ['jest-serializer-vue']
+}
